0d9c91e00831e1a9e3375c11fba2f0096ff12e16
beams <834483878@qq.com>
2023-09-08 18:02:45 +0800

    添加antf-eslint和unocss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    [
+      '@unocss/nuxt',
+      {
+        uno: true, // enabled `@unocss/preset-uno`
+        icons: true, // enabled `@unocss/preset-icons`
+        attributify: true, // enabled `@unocss/preset-attributify`,
+        // core options
+        shortcuts: [],
+        rules: [],
+        safelist: [],
+      },
+    ],
+  ],
 })
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  <h1 class="bg-blue-200">Index Page</h1>
+  <div class="i-vscode-icons-file-type-nuxt" />
+  <div class="i-solar-accumulator-bold-duotone" />
+</template>
+
+<style lang="scss"></style>
